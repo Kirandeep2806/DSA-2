b45b01fca0329cb2ab99b8e23926b370a94c72da
Kiran Deep <kirandeep102030@gmail.com>
2022-06-16 23:27:16 +0530

    Added Topological sorting skeletal

---
+++ Miscellaneous/topologicalSorting.py
@@ -0,0 +1,10 @@
+nodes = int(input())
+
+def DFS(adjMatrix, visited=set(), stack=[]):
+    pass
+
+adjMatrix = []
+for i in range(nodes):
+    adjMatrix.append(list(map(int, input(f"Enter the adjacent nodes for {i+1} : ").split())))
+
+
