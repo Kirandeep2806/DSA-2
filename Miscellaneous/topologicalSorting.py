142232cd8e80c6f8f0cebb1502d163f3f15ea1c7
Kiran Deep <kirandeep102030@gmail.com>
2022-07-05 21:12:36 +0530

    Added topological sorting

--- Miscellaneous/topologicalSorting.py
+++ Miscellaneous/topologicalSorting.py
@@ -1,10 +1,19 @@
-nodes = int(input())
+#!/usr/bin/python3
 
-def DFS(adjMatrix, visited=set(), stack=[]):
-    pass
+adjMatrix = [[0,0,0,0,0,0], [0,0,0,0,0,0],[0,0,0,1,0,0],[0,1,0,0,0,0],[1,1,0,0,0,0],[1,0,1,0,0,0]]
+visited = {}
+stack = []
 
-adjMatrix = []
-for i in range(nodes):
-    adjMatrix.append(list(map(int, input(f"Enter the adjacent nodes for {i+1} : ").split())))
+def DFS(adjMatrix, currNode, visited):
+    global stack
+    visited[currNode] = True
+    stack.append(currNode)
+    for pos, i in enumerate(adjMatrix[currNode]):
+        if i and (not visited.get(pos, 0)):
+            DFS(adjMatrix, pos, visited)
 
+for vertex, i in enumerate(adjMatrix):
+    if vertex not in visited.keys():
+        DFS(adjMatrix, vertex, visited)
 
+print(stack[::-1])
