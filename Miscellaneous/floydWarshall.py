29bbc7162e04d6c12cc1673c70dd9e80c08493e2
Kiran Deep <kirandeep102030@gmail.com>
2022-07-06 00:37:57 +0530

    Added Floyd Warshall

---
+++ Miscellaneous/floydWarshall.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import graphlib
+from sys import maxsize as MAX_VALUE
+
+V = 4
+
+
+def floydWarshall(graph):
+    for k in range(V):
+        for i in range(V):
+            for j in range(V):
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+    return graph
+
+
+graph = [[0, 5, MAX_VALUE, 10],
+         [MAX_VALUE, 0, 3, MAX_VALUE],
+         [MAX_VALUE, MAX_VALUE, 0, 1],
+         [MAX_VALUE, MAX_VALUE, MAX_VALUE, 0]
+         ]
+
+res = floydWarshall(graph)
+
+for vertex in res:
+    for i in vertex:
+        if i == MAX_VALUE:
+            print("INF", end=" ")
+        else:
+            print(i, end=" ")
+    print()
+
+# print(res)
